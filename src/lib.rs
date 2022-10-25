7643ffd00d1001f7f004deffb8054ea55b73dee8
DevAles <devalesoficial@gmail.com>
2022-10-25 18:59:12 -0300

    buffer I/O

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+use std::io::stdin;
+use std::io::Read;
+
 pub fn run() {
-    todo!();
+    let mut buffer = [0; 1];
+
+    while stdin().read(&mut buffer).expect("Failed to read line") == 1 && buffer[0] != b'q' {}
 }
